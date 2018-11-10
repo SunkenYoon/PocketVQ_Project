16c87dca73e2a287baac1974f8530169f38e4696
SunkenYoon <44047085+SunkenYoon@users.noreply.github.com>
2018-11-10 14:07:23 +0900

    CustomListView2
    Applied customlist to Kor/Eng xml files in Vocab.java

--- App1/app/src/main/java/com/example/ysg/app1/Shared.java
+++ App1/app/src/main/java/com/example/ysg/app1/Shared.java
@@ -2,29 +2,48 @@ package com.example.ysg.app1;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.widget.ListView;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
 
 public class Shared extends Activity {
+    ListView listview ;
+    ListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shared);
-        ListView listview ;
-        ListAdapter adapter;
         adapter = new ListAdapter() ;
         listview = (ListView) findViewById(R.id.listview);
         listview.setAdapter(adapter);
-        adapter.addItem(
-                "Box", "Account Box Black 36dp") ;
-        // 두 번째 아이템 추가.
-        adapter.addItem(
-                "Circle", "Account Circle Black 36dp") ;
-        // 세 번째 아이템 추가.
-        adapter.addItem(
-                "Ind", "Assignment Ind Black 36dp") ;
+        Excel();
+    }
+    public void Excel() {
+        Workbook workbook = null;
+        Sheet sheet = null;
+        try {
+            InputStream inputStream = getBaseContext().getResources().getAssets().open("first.xls");
+            workbook = Workbook.getWorkbook(inputStream);
+            sheet = workbook.getSheet(0);
+            int MaxColumn = 2, RowStart = 0, RowEnd = sheet.getColumn(MaxColumn - 1).length - 1, ColumnStart = 0, ColumnEnd = sheet.getRow(2).length - 1;
+            for (int row = RowStart; row <= RowEnd; row++) {
+                String excelload1 = sheet.getCell(ColumnStart, row).getContents();
+                String excelload2 = sheet.getCell(ColumnEnd, row).getContents();
+                adapter.addItem(excelload1, excelload2);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } finally {
+            listview.setAdapter(adapter);
+            workbook.close();
+        }
     }
 }
--- App1/app/src/main/java/com/example/ysg/app1/Vocab.java
+++ App1/app/src/main/java/com/example/ysg/app1/Vocab.java
@@ -1,25 +1,27 @@
 package com.example.ysg.app1;
 
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import java.io.IOException;
 import java.io.InputStream;
+
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
-public class Vocab extends AppCompatActivity {
-    ListView list;
-    ArrayAdapter<String> arrayAdapter;
+
+public class Vocab extends Activity {
+    ListView listview ;
+    ListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vocab);
-        list = (ListView)findViewById(R.id.list);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1);
+        setContentView(R.layout.shared);
+        adapter = new ListAdapter() ;
+        listview = (ListView) findViewById(R.id.listview);
+        listview.setAdapter(adapter);
         Excel();
     }
     public void Excel() {
@@ -29,17 +31,18 @@ public class Vocab extends AppCompatActivity {
             InputStream inputStream = getBaseContext().getResources().getAssets().open("first.xls");
             workbook = Workbook.getWorkbook(inputStream);
             sheet = workbook.getSheet(0);
-            int MaxColumn = 2, RowStart = 0, RowEnd = sheet.getColumn(MaxColumn - 1).length -1, ColumnStart = 0, ColumnEnd = sheet.getRow(2).length - 1;
-            for(int row = RowStart;row <= RowEnd;row++) {
-                String excelload = sheet.getCell(ColumnStart, row).getContents();
-                arrayAdapter.add(excelload);
+            int MaxColumn = 2, RowStart = 0, RowEnd = sheet.getColumn(MaxColumn - 1).length - 1, ColumnStart = 0, ColumnEnd = sheet.getRow(2).length - 1;
+            for (int row = RowStart; row <= RowEnd; row++) {
+                String excelload1 = sheet.getCell(ColumnStart, row).getContents();
+                String excelload2 = sheet.getCell(ColumnEnd, row).getContents();
+                adapter.addItem(excelload1, excelload2);
             }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (BiffException e) {
             e.printStackTrace();
         } finally {
-            list.setAdapter(arrayAdapter);
+            listview.setAdapter(adapter);
             workbook.close();
         }
     }
